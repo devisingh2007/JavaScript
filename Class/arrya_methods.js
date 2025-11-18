79bb257a5e7c7a1d96bf63888001ce05682e939b
devisingh rajput <devisinghrajput.cg@gmail.com>
2025-11-18 11:41:51 +0530

    Array methods

---
+++ Class/arrya_methods.js
@@ -0,0 +1,40 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.length=2;                                                //set arrya length
+// let size = fruits.length;                                      //check array length
+// console.log(fruits)
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let myList = fruits.toString();                                   //chenge array to string
+// console.log(myList)
+
+
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fruit = fruits.at(2);                                             //Get the third element of fruits using at()
+// console.log(fruit)
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fruit = fruits[2];                                            //Get the third element of fruits modan  way
+// console.log(fruit)
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// var mm = fruits.join(" * ");                                          //The join() method also joins all array elements into a string. It behaves just like toString(), but in addition you can specify the separator:
+// console.log(mm)
+
+
+
+// let arr = [2, 3, 4, [5,[    [89, 90,        [25, 36, 8, 8, 87, 6, 39]        , 25, 8, 8, 9, 3, 8,],             2588, 77, 9, 2, 57, 7, 89, 3, 787] , 28863, 2, 8777, 3, 6, 999, 44]    , "errr"];
+// const newArr = arr.flat(6);
+// console.log(newArr)
+
+
+
+let arr=[2,3,4,6,"errг",3,4,6,"errг",2,3,4,6,"errг",3,4,6,"errг"];
+let arr1=[34,45,67]
+let str= arr.splice(15);
+console.log(arr);
+
+
