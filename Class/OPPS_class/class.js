679b6b10bfa427c41755c20502b10d4720158b83
devisingh rajput <devisinghrajput.cg@gmail.com>
2025-12-23 15:36:19 +0530

    basic class example

---
+++ Class/OPPS_class/class.js
@@ -0,0 +1,14 @@
+class car{
+    constructor(company,make,egine)
+    {
+        this.company=company;
+        this.make=make;
+        this.egine=egine;   
+    }
+}
+
+var obj1=new car("Toyota","Innova","V6");
+console.log(obj1);
+
+var obj2=new car("Honda","Civic","V4");
+console.log(obj2);
